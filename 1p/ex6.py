3a9aa2045957c06a4f1638966cd59a3f9c649a96
vporret <47745426+vporret@users.noreply.github.com>
2019-03-16 13:28:10 +0100

    Add files via upload

--- 1p/ex6.py
+++ 1p/ex6.py
@@ -1,16 +1,12 @@
 # Exercice 6
 
-def c(f) :
-    c = ((f) - 32) * 5/9
-    c = round(c,2)
-    print(str(f) + " Fahrenheit degrees = " + str(c) + " degrees Celsius")
+# Celsius to Fahrenheit
 
-x = (c(50))**(-1)
-print(x)
-# c == c(c(5.7))
-#print(t)
-
-
-    
-
-
+def C(F) :
+    C = F
+    F = (9/5)*C +32
+    print(str(C) + " degrees Celsius = "+ str(F) +" degrees Fahrenheit")
+    C1 = (5/9) * (F-32)
+    if C1 == C:
+        print("Boolean test : true")
+C(50)    
