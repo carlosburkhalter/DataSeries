df36bba030d47fd1ad532f90f7ea1e999e62c874
vporret <47745426+vporret@users.noreply.github.com>
2019-03-20 14:47:07 +0100

    Delete ex9.py

--- 1p/ex9.py
+++
@@ -1,9 +0,0 @@
-# Exercice 9
-
-from math import sqrt, pi, exp
-def gauss_function(x):
-    return (1/sqrt(2*pi))*exp(-0.5*((x)**2))
-    
-for x in range(-5,6,1):
-    z=gauss_function(x)
-    print(z)
